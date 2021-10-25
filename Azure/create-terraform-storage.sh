65944cc5747d146c7788672ac9dc009017acd754
Andrew Campbell <andrewbcampbell@outlook.com>
2021-10-25 14:50:10 +0000

    add ubuntu container

--- Azure/create-terraform-storage.sh
+++ Azure/create-terraform-storage.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-RESOURCE_GROUP_NAME="devopstamops-rg"
-STORAGE_ACCOUNT_NAME="devopstamopssa"
+RESOURCE_GROUP_NAME="rg-devops-hard"
+STORAGE_ACCOUNT_NAME="sadevopshard88392"
+LOCATION = "eastus"
 
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+az group create -l $LOCATION -n $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l $LOCATION --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
