9f4b925f801edce78ec117fd68daa5902a3ca1a9
Thomas Thornton <thomas@thomasthornton.cloud>
2024-03-19 20:59:05 +0000

    fix rbac

--- Terraform-AZURE-Services-Creation/AKS/rbac.tf
+++ Terraform-AZURE-Services-Creation/AKS/rbac.tf
@@ -15,7 +15,7 @@ resource "azurerm_role_assignment" "node_infrastructure_update_scale_set" {
 }
 
 data "azurerm_container_registry" "acr" {
-  name                = "${var.name}acr"
+  name                = "${var.name}tamopsacracr"
   resource_group_name = data.azurerm_resource_group.resource_group.name
 }
 
@@ -26,16 +26,4 @@ resource "azurerm_role_assignment" "acr_pull" {
   depends_on = [
     azurerm_kubernetes_cluster.k8s
   ]
-}
-
-# resource "azurerm_role_assignment" "appgateway_noderg_contributor" {
-#   scope                = data.azurerm_resource_group.node_resource_group.id
-#   role_definition_name = "Contributor"
-#   principal_id         = azurerm_kubernetes_cluster.k8s.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
-# }
-
-resource "azurerm_role_assignment" "appgateway_aksrg_contributor" {
-  scope                = data.azurerm_resource_group.resource_group.id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_kubernetes_cluster.k8s.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
 }
