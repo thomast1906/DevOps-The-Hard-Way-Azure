f55b4eb12ca8e08f75f38fcf8bf84491156e4d90
Thomas Thornton <>
2024-03-22 16:22:57 +0000

    fix id

--- Terraform-AZURE-Services-Creation/AKS/rbac.tf
+++ Terraform-AZURE-Services-Creation/AKS/rbac.tf
@@ -31,7 +31,7 @@ resource "azurerm_role_assignment" "acr_pull" {
 resource "azurerm_role_assignment" "appgwcontainer" {
   principal_id       = azurerm_user_assigned_identity.alb_identity.principal_id
   scope              = data.azurerm_resource_group.resource_group.id
-  role_definition_id = "reader"
+  role_definition_name = "reader"
   depends_on = [
     azurerm_kubernetes_cluster.k8s,
     azurerm_user_assigned_identity.alb_identity
