87ae72c094800ca28fe86e21c14eda33b9ef929d
thomast1906 <thomast1906@users.noreply.github.com>
2024-03-22 16:23:22 +0000

    Terraform fmt

--- Terraform-AZURE-Services-Creation/AKS/rbac.tf
+++ Terraform-AZURE-Services-Creation/AKS/rbac.tf
@@ -29,8 +29,8 @@ resource "azurerm_role_assignment" "acr_pull" {
 }
 
 resource "azurerm_role_assignment" "appgwcontainer" {
-  principal_id       = azurerm_user_assigned_identity.alb_identity.principal_id
-  scope              = data.azurerm_resource_group.resource_group.id
+  principal_id         = azurerm_user_assigned_identity.alb_identity.principal_id
+  scope                = data.azurerm_resource_group.resource_group.id
   role_definition_name = "reader"
   depends_on = [
     azurerm_kubernetes_cluster.k8s,
