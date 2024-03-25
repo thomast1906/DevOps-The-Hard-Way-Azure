f4246e8bc19cb5984212d649addde24236995736
Thomas Thornton <>
2024-03-25 16:11:07 +0000

    terraform changes

--- Terraform-AZURE-Services-Creation/VNET/vnet.tf
+++ Terraform-AZURE-Services-Creation/VNET/vnet.tf
@@ -22,7 +22,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = data.azurerm_resource_group.resource_group.name
   address_space       = [var.network_address_space]
 
-    tags = var.tags
+  tags = var.tags
 
 }
 
@@ -39,12 +39,13 @@ resource "azurerm_subnet" "app_gwsubnet" {
   virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = [var.subnet_address_prefix]
 
-    delegation {
+  delegation {
     name = "delegation"
 
     service_delegation {
       name    = "Microsoft.ServiceNetworking/trafficControllers"
-     
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+
     }
   }
 }
